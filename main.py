92bc6355cb33ddc03f7d836f2f52670b699c807c
megalab <mega1ab@ya.ru>
2022-10-03 02:14:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+
+# Подкласс QMainWindow для настройки главного окна приложения
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        # Устанавливаем центральный виджет Window.
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+
+sys.exit(app.exec())
